8c9b949f6709b9d7c00458c7c34dd72ddef79e3b
Evgeny Gerashchenko <Evgeny.Gerashchenko@jetbrains.com>
2012-03-01 20:02:30 +0400

    Added COMPACT descriptor renderer (without "defined in ...")

--- compiler/frontend/src/org/jetbrains/jet/resolve/DescriptorRenderer.java
+++ compiler/frontend/src/org/jetbrains/jet/resolve/DescriptorRenderer.java
@@ -35,6 +35,13 @@ import java.util.List;
  */
 public class DescriptorRenderer implements Renderer<DeclarationDescriptor> {
 
+    public static final DescriptorRenderer COMPACT = new DescriptorRenderer() {
+        @Override
+        protected boolean shouldRenderDefinedIn() {
+            return false;
+        }
+    };
+
     public static final DescriptorRenderer TEXT = new DescriptorRenderer();
 
     public static final DescriptorRenderer HTML = new DescriptorRenderer() {
@@ -101,8 +108,14 @@ public class DescriptorRenderer implements Renderer<DeclarationDescriptor> {
         if (declarationDescriptor == null) return lt() + "null>";
         StringBuilder stringBuilder = new StringBuilder();
         declarationDescriptor.accept(rootVisitor, stringBuilder);
-        appendDefinedIn(declarationDescriptor, stringBuilder);
+        if (shouldRenderDefinedIn()) {
+            appendDefinedIn(declarationDescriptor, stringBuilder);
+        }
         return stringBuilder.toString();
+    }
+
+    protected boolean shouldRenderDefinedIn() {
+        return true;
     }
 
     private void appendDefinedIn(DeclarationDescriptor declarationDescriptor, StringBuilder stringBuilder) {
@@ -117,7 +130,9 @@ public class DescriptorRenderer implements Renderer<DeclarationDescriptor> {
     public String renderAsObject(@NotNull ClassDescriptor classDescriptor) {
         StringBuilder stringBuilder = new StringBuilder();
         rootVisitor.renderClassDescriptor(classDescriptor, stringBuilder, "object");
-        appendDefinedIn(classDescriptor, stringBuilder);
+        if (shouldRenderDefinedIn()) {
+            appendDefinedIn(classDescriptor, stringBuilder);
+        }
         return stringBuilder.toString();
     }
 
@@ -258,7 +273,7 @@ public class DescriptorRenderer implements Renderer<DeclarationDescriptor> {
                         builder.append(", ");
                     }
                 }
-                builder.append("> ");
+                builder.append(">");
             }
         }
 
